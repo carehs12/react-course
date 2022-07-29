4dd199bb7247bbf43e69c375858bae4ae0dbbd70
volant360 <efrainh12@galileo.edu>
2022-07-29 10:57:20 -0600

    feat: added state management to update title on click

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,30 @@
-import "./ExpenseItem.css";
-
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../IU/Card";
 
+import "./ExpenseItem.css";
+
 function ExpenseItem(props) {
+   // useStates returns 2 values, the variable with the desired value, 
+   // and an update function that we can call to update that value.
+   // @param initial_value - The initial value to be assigned to the variable
+   // @return Array(2) - An array with 2 object: The first one is the variable and the 
+   // second one is the update function for that variable 
+   const [title, setTitle] = useState(props.title);
+
+   const clickHandler = () => {
+      setTitle("Title changed!");
+      console.log(title);
+   };
+
    return (
       <Card className="expense-item">
          <ExpenseDate date={props.date} />
          <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">${props.amount}</div>
          </div>
+         <button onClick={clickHandler}>Change Titles</button>
       </Card>
    );
 }
